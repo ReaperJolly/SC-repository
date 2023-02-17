c537c7c50785c18a5306645b145371498c4dace6
dragan.kukolj <kukolj.dragan@gmail.com>
2023-02-17 17:50:31 +0100

    Commit

--- 03-gulp/gulpfile.js
+++ 03-gulp/gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require("gulp");
 const babel = require("gulp-babel");
 const uglify = require("gulp-uglify");
+const sass = require("gulp-sass")(require("node-sass"));
+const cssNano = require("gulp-cssnano");
 
 gulp.task("js", function () {
     return gulp.src("src/*.js")
@@ -8,3 +10,15 @@ gulp.task("js", function () {
         .pipe(uglify())
         .pipe(gulp.dest("dist/"));
 });
+
+gulp.task("sass", function () {
+    return gulp.src("src/style.scss")
+    .pipe(sass())
+    .pipe(cssNano())
+    .pipe(gulp.dest("dist/"));
+});
+
+gulp.task("watch", function() {
+    gulp.watch(["src/index.js"], gulp.series("js"));
+    gulp.watch(["src/style.scss"], gulp.series("sass"));
+})
